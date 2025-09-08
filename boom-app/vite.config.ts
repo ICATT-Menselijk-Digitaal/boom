65401e411ab24a952cebea84bab1cf1b9fce5e13
Daan Gast <daangast@gmail.com>
2025-09-08 16:25:58 +0200

    Proxy setup

--- boom-app/vite.config.ts
+++ boom-app/vite.config.ts
@@ -1,18 +1,37 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  server: {
+    proxy: {
+      '/objects-api': {
+        target: 'http://localhost:8000/api/v2/',
+        changeOrigin: true,
+        headers: {
+          Authorization: `Token ${loadEnv('env', process.cwd(), '').VITE_OBJECTS_API_KEY}`,
+          Cookie: '',
+        },
+        rewrite: (path) => path.replace(/^\/objects-api/, ''),
+      },
+      '/objecttypes-api': {
+        target: 'http://localhost:8001/api/v2/',
+        changeOrigin: true,
+        headers: {
+          Authorization: `Token ${loadEnv('env', process.cwd(), '').VITE_OBJECTTYPES_API_KEY}`,
+          Cookie: '',
+        },
+        rewrite: (path) => path.replace(/^\/objecttypes-api/, ''),
+      },
+    },
+  },
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
