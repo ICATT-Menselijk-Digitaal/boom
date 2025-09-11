fec9fdec47c9086ad61077c5dcb16f39a93abeea
Daan Gast <daangast@gmail.com>
2025-09-11 14:50:56 +0200

    Added types for API-calls

--- boom-app/src/types.ts
+++ boom-app/src/types.ts
@@ -15,3 +15,27 @@ export type ObjectType = {
 
 export type CsvRecord = Record<string, string>
 export type Mapping = Record<string, string>
+
+export type PaginatedObjectTypeList = {
+  count: number
+  next?: string | null
+  previous?: string | null
+  results: ObjectTypeResponse[]
+}
+export type ObjectTypeResponse = {
+  readonly url?: string
+  uuid?: string
+  name: string
+  readonly versions?: string[]
+}
+
+export type ObjectTypeVersion = {
+  readonly url?: string
+  readonly version?: number
+  readonly objectType?: string
+  status?: 'published' | 'draft' | 'deprecated'
+  jsonSchema?: unknown
+  readonly createdAt?: string
+  readonly modifiedAt?: string
+  readonly publishedAt?: string | null
+}
