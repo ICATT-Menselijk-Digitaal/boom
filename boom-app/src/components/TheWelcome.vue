ea17dcfcd4c8e6ea2c5ea3ef91d70809537f7407
Daan Gast <daangast@gmail.com>
2025-10-22 15:35:38 +0200

    Changed intro text

--- boom-app/src/components/TheWelcome.vue
+++ boom-app/src/components/TheWelcome.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="flex column box">
-    <p>Welcome BOOMer!</p>
-    <p>You can use this app to add Overige Objecten in bulk into the database.</p>
-    <p>It is simple to use, just follow the flow.</p>
+    <p>BOOM stands for <b>B</b>ulk <b>O</b>verige <b>O</b>bjecten <b>M</b>apping</p>
+    <p>You can use this app to add new objects in bulk into a Overige Objecten database.</p>
+    <p>You will walk through the following steps:</p>
+    <ol>
+      <li>You will start with uploading a CSV file of the data that needs to be addded.</li>
+      <li>
+        The second step is to select the Object-type and -version that describes the format of your
+        new objects.
+      </li>
+      <li>
+        Then a mapping will be done by selected which column of the CSV data matches the selected
+        Objecttype properties.
+      </li>
+      <li>
+        Before entering new objects, a preview will be displayed to check if the mapping was done
+        correctly.
+      </li>
+    </ol>
+    <p>To start press the <b>Next</b> button</p>
   </div>
 </template>
 
